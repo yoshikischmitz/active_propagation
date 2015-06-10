46728e37f97767cf944363c7c5bed23d48a4a3f4
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 16:54:33 -0600

    failing spec

--- spec/active_propagation_spec.rb
+++ spec/active_propagation_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe ActivePropagation do
+  describe "activerecord extensions" do
+    it "should respond to propgate_changes" do
+      expect(Post).to respond_to(:propagates_changes_to)
+    end
+  end
 end
