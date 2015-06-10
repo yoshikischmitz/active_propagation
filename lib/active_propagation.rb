6d3bd68e0ac2b0d03516f31d318cf50226d5f1f2
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 17:06:51 -0600

    rename file

--- lib/active_propagation.rb
+++ lib/active_propagation.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 require "active_propagation/version"
-require "active_propagation/extensions"
+require "active_propagation/class_extensions"
 
 module ActivePropagation
 end
