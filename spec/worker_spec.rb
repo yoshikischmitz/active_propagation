1c25a091f13a6ff6a1f13f8d968c9dd8cd2cc9ef
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 20:13:57 -0600

    spec for worker

---
+++ spec/worker_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe ActivePropagation::Worker do
+  describe "perform" do
+    before do
+      @model = Post.create
+      @propagater_mock = double 
+      allow(@propagater_mock).to receive(:run)
+    end
+
+    it "should instantiate a synchronous worker with the correct args" do
+      allow(ActivePropagation::Propagater).to receive(:new).and_return(@propagater_mock)
+      ActivePropagation::Worker.new.perform("Post", @model.id, :posts, [:text], false)
+      expect(ActivePropagation::Propagater).to have_received(:new).with(@model, :posts, only: [:text])
+      expect(@propagater_mock).to have_received(:run)
+    end
+  end
+end
