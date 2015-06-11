792acf356a6016200cf067eeaa512e645b9be9cd
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 19:08:31 -0600

    add spec for proper instantiation

--- spec/active_propagation_spec.rb
+++ spec/active_propagation_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 require 'pry'
+class Post
+  propagates_changes_to :posts, only: [:text]
+end
 
 describe ActivePropagation do
   describe "activerecord extensions" do
@@ -14,17 +17,25 @@ describe ActivePropagation do
     end
   end
 
-  class Post
-    propagates_changes_to :posts, only: [:text]
-  end
-
   describe "_run_active_propagation" do
     before do
       @post = Post.create
     end
+
     it "should instantiate a synchronous propagater with synchronous propagation by default" do
       expect_any_instance_of(ActivePropagation::Propagater).to receive(:run)
       @post._run_active_propagation
+    end
+
+    class PropagaterMock
+      def run
+      end
+    end
+
+    it "should instantiate a synchronous propagater with synchronous propagation by default" do
+      allow(ActivePropagation::Propagater).to receive(:new).and_return(PropagaterMock.new)
+      @post._run_active_propagation
+      expect(ActivePropagation::Propagater).to have_received(:new).with(@post, :posts, only: [:text])
     end
   end
 end
