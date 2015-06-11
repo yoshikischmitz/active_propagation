9cba64f6a90537473b1503be9cc53b5165d6e8b7
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 19:24:53 -0600

    reset the class variable each time

--- spec/active_propagation_spec.rb
+++ spec/active_propagation_spec.rb
@@ -1,24 +1,40 @@
 require 'spec_helper'
 require 'pry'
-class Post
-  propagates_changes_to :posts, only: [:text]
-end
 
 describe ActivePropagation do
+  after(:each) do
+    Post.class_variable_set(:@@propagations, nil)
+  end
+
   describe "activerecord extensions" do
+    before do
+      Post.send(:propagates_changes_to, :posts, only: [:text])
+    end
+
     it "should register propgate_changes_to as a class method" do
       expect(Post).to respond_to(:propagates_changes_to)
     end
   end
 
   describe "it should register callbacks" do
+    before do
+      Post.send(:propagates_changes_to, :posts, only: [:text])
+    end
+
     it "should register the correct callback" do
       expect(Post._commit_callbacks.map(&:filter)).to include(:_run_active_propagation)
+    end
+
+    it "should register the callback once" do
+      Post.send(:propagates_changes_to, :posts, only: [:text])
+      Post.send(:propagates_changes_to, :posts, only: [:text])
+      expect(Post._commit_callbacks.map(&:filter).select{|x| x == :_run_active_propagation}.size).to eq(1)
     end
   end
 
   describe "_run_active_propagation" do
     before do
+      Post.send(:propagates_changes_to, :posts, only: [:text])
       @post = Post.create
     end
 
@@ -36,6 +52,9 @@ describe ActivePropagation do
       allow(ActivePropagation::Propagater).to receive(:new).and_return(PropagaterMock.new)
       @post._run_active_propagation
       expect(ActivePropagation::Propagater).to have_received(:new).with(@post, :posts, only: [:text])
+    end
+
+    it "only runs one job per key" do
     end
   end
 end
