5ce6ae6e29d1b40b4ce39d56ec00c339ef126a65
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 19:34:48 -0600

    proper use config for worker

--- lib/active_propagation/instance_extensions.rb
+++ lib/active_propagation/instance_extensions.rb
@@ -3,7 +3,7 @@ module ActivePropagation::InstanceExtensions
     klass = self.class
     klass.class_variable_get(:@@propagations).each do |association, config|
       if config[:async]
-        ActivePropagation::Worker.perform_async(self.class.to_s, self.id, association.to_s, only, nested_async)
+        ActivePropagation::Worker.perform_async(self.class.to_s, self.id, association.to_s, config[:only], config[:nested_async])
       else
         ActivePropagation::PROPAGATERS[config[:nested_async]].new(self, association, only: config[:only]).run
       end
