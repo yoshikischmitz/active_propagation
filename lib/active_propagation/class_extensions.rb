f18abcaaefee277d393de44fc04fcd961cb78c79
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 18:53:22 -0600

    metaprogram in the class variables

--- lib/active_propagation/class_extensions.rb
+++ lib/active_propagation/class_extensions.rb
@@ -1,8 +1,7 @@
 module ActivePropagation::ClassExtensions
   def propagates_changes_to(association, only: [], async: false, nested_async: false, on: [:update, :destroy])
-    @@propagations ||= []
+    self.class_variable_set(:@@propagations, [])
     after_commit(:_run_active_propagation, on: on)
-    @@propagations << [association, only, async, nested_async]
+    self.class_variable_get(:@@propagations) << [association, only, async, nested_async]
   end
 end
-
--- lib/active_propagation/instance_extensions.rb
+++ lib/active_propagation/instance_extensions.rb
@@ -1,4 +1,12 @@
 module ActivePropagation::InstanceExtensions
   def _run_active_propagation
+    klass = self.class
+    klass.class_variable_get(:@@propagations).each do |(association, only, async, nested_async)|
+      if async
+        ActivePropagation::Worker.perform_async(self.class.to_s, self.id, association.to_s, only, nested_async)
+      else
+        ActivePropagation::PROPAGATERS[nested_async].new(self, association, only: only).run
+      end
+    end
   end
 end
