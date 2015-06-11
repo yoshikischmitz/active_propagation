92eec919ca10f25a0ba032846ed13fc2c9ab7f94
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 20:01:16 -0600

    add spec for async

--- spec/propagater_spec.rb
+++ spec/propagater_spec.rb
@@ -6,6 +6,10 @@ end
 class ModelMock
   attr_accessor :association
 
+  def id
+    1
+  end
+
   def attributes
     {stuff: :things}
   end
@@ -13,9 +17,6 @@ end
 
 RSpec.describe ActivePropagation::Propagater do
   describe "#run" do
-    before do 
-    end
-
     it "should update each of the associations with the same data" do
       model = ModelMock.new
       association1 = double
@@ -28,6 +29,28 @@ RSpec.describe ActivePropagation::Propagater do
       @propagater.run
       expect(association1).to have_received(:update).with({stuff: :things})
       expect(association2).to have_received(:update).with({stuff: :things})
+    end
+  end
+end
+
+RSpec.describe ActivePropagation::AsyncPropagater do
+  describe "#run" do
+    it "should update each of the associations with the same data" do
+      model = ModelMock.new
+      association1 = double
+      association2 = double
+      allow(association1).to receive(:update)
+      allow(association2).to receive(:update)
+      allow(association1).to receive(:id).and_return 1
+      allow(association2).to receive(:id).and_return 2
+      model.association = [association1, association2]
+
+      allow(ActivePropagation::LoopWorker).to receive(:perform_async)
+
+      @propagater = ActivePropagation::AsyncPropagater.new(model, :association, only: [:stuff])
+      @propagater.run
+      expect(ActivePropagation::LoopWorker).to have_received(:perform_async).with("ModelMock", 1, 1, [:stuff])
+      expect(ActivePropagation::LoopWorker).to have_received(:perform_async).with("ModelMock", 1, 2, [:stuff])
     end
   end
 end
