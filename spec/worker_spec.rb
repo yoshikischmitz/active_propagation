2aeff2c30dea96fb52833863e1ddba610db9befa
Yoshiki <yoshiki@masteryconnect.com>
2015-06-10 20:14:56 -0600

    add spec for async prop

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -12,5 +12,12 @@ RSpec.describe ActivePropagation::Worker do
       expect(ActivePropagation::Propagater).to have_received(:new).with(@model, :posts, only: [:text])
       expect(@propagater_mock).to have_received(:run)
     end
+
+    it "should instantiate an asynchronous worker with the correct args" do
+      allow(ActivePropagation::AsyncPropagater).to receive(:new).and_return(@propagater_mock)
+      ActivePropagation::Worker.new.perform("Post", @model.id, :posts, [:text], true)
+      expect(ActivePropagation::AsyncPropagater).to have_received(:new).with(@model, :posts, only: [:text])
+      expect(@propagater_mock).to have_received(:run)
+    end
   end
 end
