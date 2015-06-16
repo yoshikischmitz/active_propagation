afe4d3ac3274f4503ab3392b0dc5059990f9abc8
Yoshiki <yoshiki@masteryconnect.com>
2015-06-16 10:44:10 -0600

    switch logic

--- lib/active_propagation/class_extensions.rb
+++ lib/active_propagation/class_extensions.rb
@@ -1,6 +1,6 @@
 module ActivePropagation::ClassExtensions
   def propagates_changes_to(association, only: [], async: false, nested_async: false, on: [:update, :destroy])
-    self.class_variable_set(:@@propagations, {}) unless self.class_variable_defined?(:@@propagations)
+    self.class_variable_set(:@@propagations, self.class_variable_get(:@@propagations) || {})
     after_commit(:_run_active_propagation, on: on) unless _commit_callbacks.map(&:filter).include?(:_run_active_propagation)
     self.class_variable_get(:@@propagations)[association] = {only: only, async: async, nested_async: nested_async}
   end
