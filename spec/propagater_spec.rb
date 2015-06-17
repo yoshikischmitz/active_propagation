7b36d6634d5e2bc20fd60ae023d00e907a3958d0
Yoshiki <yoshiki@masteryconnect.com>
2015-06-17 10:58:47 -0600

    tests for deletion

--- spec/propagater_spec.rb
+++ spec/propagater_spec.rb
@@ -23,3 +23,27 @@ RSpec.describe "Updaters" do
     end
   end
 end
+
+RSpec.describe "Deletors" do
+  before do
+    @post = Post.create(text: "hello world")
+    @remote1 = Post.create(post: @post)
+    @remote2 = Post.create(post: @post)
+  end
+
+  describe "Synchronous Deletor" do
+    it "should update each of the associations with the same data" do
+      ActivePropagation::Deletor.run(@post.class.to_s, @post.id, :posts, [:text])
+      expect(Post.exists?(@remote1.id)).to eq(false)
+      expect(Post.exists?(@remote2.id)).to eq(false)
+    end
+  end
+
+  describe "Async Updater" do
+    it "should update each of the associations with the same data" do
+      ActivePropagation::AsyncDeletor.new.perform(@post.class.to_s, @post.id, :posts, [:text])
+      expect(Post.exists?(@remote1.id)).to eq(false)
+      expect(Post.exists?(@remote2.id)).to eq(false)
+    end
+  end
+end
