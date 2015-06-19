69912e8c8234f9a57536f0d46eea24d8e42db737
Yoshiki <yoshiki@masteryconnect.com>
2015-06-19 11:43:23 -0600

    check if class variable is defined AND not nil

--- lib/active_propagation/class_extensions.rb
+++ lib/active_propagation/class_extensions.rb
@@ -1,6 +1,11 @@
 module ActivePropagation::ClassExtensions
   def propagates_changes_to(association, only: [], async: false, nested_async: false, on: [:update, :destroy])
-    self.class_variable_set(:@@propagations, self.class_variable_get(:@@propagations) || {})
+    props = if class_variable_defined?(:@@propagations)
+      self.class_variable_get(:@@propagations) || {}
+    else
+      {}
+    end
+    self.class_variable_set(:@@propagations, props)
     after_commit(:_run_active_propagation, on: on) unless _commit_callbacks.map(&:filter).include?(:_run_active_propagation)
     self.class_variable_get(:@@propagations)[association] = {only: only, async: async, nested_async: nested_async}
   end
